4d7cb2cdc142fe347e06235edb7d27811a53360c
Alaa Apuelsoad <alaaapu135@gmail.com>
2025-06-09 21:53:37 +0300

    Event Listener

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
+import { every } from 'rxjs';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
@@ -18,6 +19,11 @@ export class UserComponent {
   get ImagePath(){
     //function using as a property
     return 'assets/users/'+this.selectedUser.avatar
+  }
+
+
+  onSelectUser(){
+    console.log('clicked');
   }
 
 }
